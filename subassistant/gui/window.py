f6131d85ccf239f6dc857d3af848b7a344a9b3c4
Disyer <daniel@tohka.us>
2024-03-26 00:02:19 +0200

    Add stylesheet on Linux

--- subassistant/gui/window.py
+++ subassistant/gui/window.py
@@ -1,7 +1,8 @@
-from PySide6.QtGui import QIcon
+from PySide6.QtGui import QIcon, QColor
 from PySide6.QtWidgets import QTabWidget, QWidget, QVBoxLayout
 from subassistant.globals import RESOURCES_DIR
 from subassistant.gui.tab import CommentTab, RemoveCommentTab, AboutTab
+from subassistant.gui import util
 import os
 
 class SubAssistantWindow(QWidget):
@@ -10,6 +11,7 @@ class SubAssistantWindow(QWidget):
         self.setWindowTitle("SubAssistant")
         self.setWindowIcon(QIcon(os.path.join(RESOURCES_DIR, 'curly_braces_icon.png')))
         self.setFixedSize(550, 350)
+        util.apply_background_color(self, QColor(240, 240, 240))
         self.initUI()
 
     def initUI(self):
@@ -22,8 +24,8 @@ class SubAssistantWindow(QWidget):
         self.tab_widget.addTab(RemoveCommentTab(), QIcon(os.path.join(RESOURCES_DIR, 'no_curly_braces.png')), "")
         self.tab_widget.addTab(AboutTab(), QIcon(os.path.join(RESOURCES_DIR, 'about.png')), "")
 
-
-        self.tab_widget.setStyleSheet("QTabBar::tab { height: 60px; width: 100px;}")
+        self.tab_widget.setObjectName("TabWidget")
+        self.tab_widget.setStyleSheet("QTabBar::tab { height: 60px; width: 100px; background-color: #f3f3f3; border: 1px solid #e8e8e8; } QTabBar::tab:selected { background-color: #f9f9f9; }")
         layout.addWidget(self.tab_widget)
 
         self.setLayout(layout)
